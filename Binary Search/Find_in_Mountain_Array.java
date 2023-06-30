51cb560f99de596100b727a67639db95d991b01d
adityadike <adityadike123@gmail.com>
2023-06-30 10:22:08 +0530

    Peak Index in a Mountain Array

---
+++ Binary Search/Find_in_Mountain_Array.java
@@ -0,0 +1,69 @@
+/** 
+ ** https://leetcode.com/problems/find-in-mountain-array/description/
+*/
+
+public class Find_in_Mountain_Array {
+/**
+ ** This is MountainArray's API interface.
+ ** You should not implement it, or speculate about its implementation
+ ** interface MountainArray {
+ **    public int get(int index) {}
+ **    public int length() {}
+ ** }
+ 
+ class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        // TODO
+        int peak = mountainArray(mountainArr);
+        int firsthalf = binarySearch(target,mountainArr,0,peak,true);
+        if(firsthalf != -1)
+        return firsthalf;
+        else 
+        return binarySearch(target,mountainArr,peak + 1,mountainArr.length()-1,false);
+    }
+    
+    // Find Peak element of Array.
+    public int mountainArray(MountainArray mountainArr){
+        // TODO
+        int start = 0;
+        int end = mountainArr.length()-1;
+        
+        while(start < end){ 
+            int mid = start + (end-start) / 2;
+            
+            if(mountainArr.get(mid) < mountainArr.get(mid+1))
+                start = mid + 1;
+            else
+            end = mid;
+        }
+        return start;
+    }
+    
+    // Find element target in Array.
+    static int binarySearch(int target, MountainArray mountainArr, int start,int end, boolean isAccending) {
+        // TODO
+        while(start <= end){
+            int mid = start + (end - start) / 2; // cause this may surpass the integer value.
+            int getMiddelElement = mountainArr.get(mid);
+            if(isAccending){ // Check for order.
+                if(getMiddelElement < target ){ 
+                    start = mid + 1;
+                }else if (getMiddelElement > target) {
+                    end = mid -1;
+                }else{
+                    return mid;
+                }
+            }else {
+                if(getMiddelElement > target ){ 
+                    start = mid + 1;
+                }else if (getMiddelElement < target) {
+                    end = mid -1;
+                }else{
+                    return mid;
+                }
+            }
+        }   
+        return -1;
+    }
+    */ 
+}
