57c10c629f42561b28868f72f7222093763ded32
adityadike <adityadike123@gmail.com>
2023-07-06 10:03:36 +0530

    Split Array Largest Sum

--- Binary Search/SplitArrayLargestSum.java
+++ Binary Search/SplitArrayLargestSum.java
@@ -10,18 +10,20 @@ public class SplitArrayLargestSum {
     public static void main(String[] args) {
         int[] nums = { 7, 2, 5, 10, 8 };
         int k = 2;
-        splitArray(nums, k);
+        int ans = splitArray(nums, k);
+        System.out.println(ans);
     }
 
     public static int splitArray(int[] nums, int k) {
         int start = 0;
         int end = 0;
-
+        // 1 <= k <= min(50, nums.length)
+        // We are getting the range of Possible answers.
         for (int i = 0; i < nums.length; i++) {
             start = Math.max(start, nums[i]);
             end += nums[i];
         }
-
+        // we are running simple binary search to search answer in range.
         while (start < end) {
             int mid = start + (end - start) / 2;
             int pieces = 1;
@@ -44,6 +46,6 @@ public class SplitArrayLargestSum {
 
         }
 
-        return -1;
+        return end;
     }
 }
