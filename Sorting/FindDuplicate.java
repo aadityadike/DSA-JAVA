edd27e9365c7d34eff25f7bd97c01f515c8296a6
aadityadike <adityadike123@gmail.com>
2023-07-17 09:37:56 +0530

    Find Duplicate Number

---
+++ Sorting/FindDuplicate.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/find-the-duplicate-number/submissions/
+
+public class FindDuplicate {
+    public static void main(String[] args) {
+        int[] nums = { 1, 3, 4, 2, 2 };
+        int ans = findDuplicate(nums);
+        System.out.println(ans);
+    }
+
+    public static int findDuplicate(int[] nums) {
+        // simple cycle sort
+        int i = 0;
+        while (nums.length > i) {
+            if (nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            } else {
+                i++;
+            }
+        }
+        // Now Array is sorted, which means duplicate element is at last index.
+        return nums[nums.length - 1];
+    }
+
+    public static void swap(int[] nums, int first, int second) {
+        int tmp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = tmp;
+    }
+}
