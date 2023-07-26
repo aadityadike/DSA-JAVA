22dbe64f0a99898ca725a4f1cd1cb44ab45c43ca
aadityadike <adityadike123@gmail.com>
2023-07-26 10:46:43 +0530

    Intermidate Pattern Questions

--- Patterns Questions/IntermediatePatterns.java
+++ Patterns Questions/IntermediatePatterns.java
@@ -1,3 +1,26 @@
 public class IntermediatePatterns {
-    
+    public static void main(String[] args) {
+        int n = 8;
+        pattern1(n);
+    }
+
+    // Combination of Right Triangles:
+    public static void pattern1(int n) {
+        int mid = n / 2;
+        for (int i = 1; i <= mid + 1; i++) {
+            // First triangle: Increasing asterisks
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = mid; i >= 1; i--) {
+            // Second triangle: Decreasing asterisks
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
