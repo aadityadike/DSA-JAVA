f07d70d764dafdd2da903e86ea13e598bc55fbe0
aadityadike <adityadike123@gmail.com>
2023-09-12 09:59:06 +0530

    LEETCODE QUESTIONS :bookmark:

--- LeetCode Problems/src/KWeakestRows.java
+++ LeetCode Problems/src/KWeakestRows.java
@@ -7,7 +7,7 @@ public class KWeakestRows {
         int[][] mat = {
                 {1, 0}, {1, 0}, {1, 0}, {1, 1}
         };
-        System.out.println(Arrays.toString(kWeakestRows(mat, 4)));
+        System.out.println(Arrays.toString(kWeakestRowsOptimise(mat, 4)));
     }
 
     public static int[] kWeakestRows(int[][] mat, int k) {
@@ -18,7 +18,7 @@ public class KWeakestRows {
         for (int[] row : mat) {
             soldiers.add(numberOfSoldiersInARow(row));
         }
-        System.out.println(soldiers);
+//        System.out.println(soldiers);
 
         int[] ans = new int[k];
         smallestElements(soldiers, ans, new ArrayList<>(), k);
@@ -55,5 +55,58 @@ public class KWeakestRows {
         }
         return count;
     }
+
+
+
+
+    public static int[] kWeakestRowsOptimise(int[][] mat, int k) {
+        int[] soldiers = new int[mat.length];
+
+        // count number of soldiers in each row by finding the ceiling of 1
+        for (int i = 0; i < mat.length; i++) {
+            soldiers[i] = ceil(mat[i]);
+        }
+
+        int[] weakOrder = new int[k];
+        // based on minimum soldiers index fill the weakOrder array
+        for (int i = 0; i < k; i++) {
+            int minIndex = min(soldiers);
+            soldiers[minIndex] = -1;
+            weakOrder[i] = minIndex;
+        }
+
+        return weakOrder;
+    }
+
+    public static int ceil(int[] row){
+        int start = 0;
+        int end = row.length-1;
+
+        while (start <= end){
+            int mid = start + (end - start)/2;
+
+            if(row[mid] < 1){
+                end = mid - 1;
+            } else {
+                start = mid +1;
+            }
+        }
+
+        return start;
+    }
+
+    public static int min(int[] arr){
+        int min = Integer.MAX_VALUE;
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] >= 0 && arr[i] < min){
+                min = arr[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
 }
+
 
